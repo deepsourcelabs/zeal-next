d0f142517b4a58b3236bd3037e6cf4acbaca5869
Yash Dave <yash@deepsource.io>
2022-05-09 15:22:26 +0530

    feat: add VNode cloning utility

---
+++ src/utils/vue-helper.ts
@@ -0,0 +1,21 @@
+import type { VNode } from 'vue'
+import { cloneVNode, Fragment } from 'vue'
+
+/**
+ * Generates an array of cloned VNodes, [since they are immutable]{@link https://github.com/vuejs/core/issues/1082#issuecomment-622519277}.
+ * Optionally, can flatten VNodes of type {@link Fragment}.
+ *
+ * @param {VNode[]} nodes - Array of VNodes
+ * @param {boolean} [flattenFragments=false] - Whether to flatten fragments or not. `false` by default.
+ */
+const cloneVNodes = (nodes: VNode[], customProps = {}, flattenFragments = false): VNode[] => {
+  return nodes.reduce((flatArray, child) => {
+    if (flattenFragments && child.type === Fragment) {
+      return cloneVNodes(child.children as VNode[], customProps)
+    }
+
+    return flatArray.concat(cloneVNode(child, customProps))
+  }, [] as VNode[])
+}
+
+export { cloneVNodes }
