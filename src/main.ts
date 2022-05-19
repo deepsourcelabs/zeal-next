4faf1ef2da1c6bc7fdcad5a73d28a84c26b51ad3
Yash Dave <yash@deepsource.io>
2022-05-19 11:45:29 +0530

    feat: add default global config for unplugin-icons

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
+//? Components
 import ZTag from './components/ZTag/ZTag.vue'
+
+//? Utils
+import { unPluginConfig } from './utils/unplugin-config'
+
+//? CSS
 import './assets/css/index.css'
 
-export { ZTag }
+export { ZTag, unPluginConfig }
---
+++ src/utils/unplugin-config.ts
@@ -0,0 +1,7 @@
+const unPluginConfig = {
+  scale: 1.2,
+  defaultClass: 'z-icon',
+  compiler: 'vue3'
+}
+
+export { unPluginConfig }
